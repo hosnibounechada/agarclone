f62d5290f3eb2039059a9b8638fe0ded598a174e
Hosni Bounechada <hosni.bounechada@gmail.com>
2022-07-14 07:47:30 +0100

    express socket.io setup

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const server = require("socket.io");
+const helmet = require("helmet");
 
 const app = express();
 
 app.use(express.static(__dirname + "/public"));
+app.use(helmet());
 
-const http = app.listen(3000, () =>
-  console.log("server is running on port 3000")
-);
+const http = app.listen(3000);
 
 const io = server(http);
+
+console.log("express and socket.io are running on port 3000");
+
+module.exports = {
+  app,
+  io,
+};
